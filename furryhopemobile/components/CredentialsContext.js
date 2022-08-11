f59a934021852883deb8ecfabdaf21b0b053c846
ChubsMentality <jericho.chubs1999@gmail.com>
2022-08-12 04:44:44 +0800

    Removed furryhopemobile

--- furryhopemobile/components/CredentialsContext.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react';
-
-// In order for all the components to access the value of the stored credentials
-// we use createContext
-export const CredentialsContext = createContext({storedCredentials: {}, setStoredCredentials: () => {}});
